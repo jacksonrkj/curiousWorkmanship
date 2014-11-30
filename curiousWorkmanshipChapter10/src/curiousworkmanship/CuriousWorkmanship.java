8a8e3081c1fded6ce86ddb78bacafd4675c0c9f0
R. Kent Jackson <jacksonk@byui.edu>
2014-11-30 13:47:43 -0700

    implemented save game and retrieve existing game

--- curiousWorkmanshipChapter10/src/curiousworkmanship/CuriousWorkmanship.java
+++ curiousWorkmanshipChapter10/src/curiousworkmanship/CuriousWorkmanship.java
@@ -11,6 +11,7 @@ import citbyui.cit260.curiousworkmanship.model.Player;
 import citbyui.cit260.curiousworkmanship.view.ErrorView;
 import citbyui.cit260.curiousworkmanship.view.StartProgramView;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
@@ -24,48 +25,62 @@ public class CuriousWorkmanship {
     private static BufferedReader inFile = null;
 
     public static void main(String[] args) { 
-    
-            try {
-                // open log file
-                String filePath = "log.txt";
-                CuriousWorkmanship.logFile = new PrintWriter(filePath);
-                CuriousWorkmanship.outFile = new PrintWriter(System.out, true);
-                CuriousWorkmanship.inFile = new BufferedReader(new InputStreamReader(System.in));
-      
-                // create StartProgramView and start the program
-                StartProgramView startProgramView = new StartProgramView();
-                startProgramView.display();
-                return; 
-                
-            } catch (Exception e) {
-                
-                // build stacktrace string.
-                StringBuilder sb = new StringBuilder();
-                for (StackTraceElement element : e.getStackTrace()) {
-                    sb.append("\t" + element.toString());
-                    sb.append("\n");
-                }
-                
-                ErrorView.display("CuriousWorkmanship",  
-                    "An abnormal error occured. "
-                    + "Exception: " + e.toString()  
-                    + "\nCause: " + e.getCause()
-                    + "\nMessage: " + e.getMessage()
-                    + "\nRestarting the program."              
-                    + "\nIf this error persist, contact support."
-                    + "\n\n" + e.getStackTrace()[0].toString()
-                    ); 
-                
-                // create StartProgramView and start the program
-                StartProgramView startProgramView = new StartProgramView();
-                startProgramView.display();
-                
-            }
+
+        try {
             
-            finally {
-                CuriousWorkmanship.logFile.close();
-                CuriousWorkmanship.outFile.close();
+            // open charcter stream files for end user input and output
+            CuriousWorkmanship.inFile = 
+                    new BufferedReader(new InputStreamReader(System.in));
+            
+            CuriousWorkmanship.outFile = new PrintWriter(System.out, true);
+        
+            // open log file
+            String filePath = "log.txt";
+            CuriousWorkmanship.logFile = new PrintWriter(filePath);
+
+
+            // create StartProgramView and start the program
+            StartProgramView startProgramView = new StartProgramView();
+            startProgramView.display();
+            return; 
+
+        } catch (Exception e) {
+
+            // build stacktrace string.
+            StringBuilder sb = new StringBuilder();
+            for (StackTraceElement element : e.getStackTrace()) {
+                sb.append("\t" + element.toString());
+                sb.append("\n");
             }
+
+            ErrorView.display("CuriousWorkmanship",  
+                "An abnormal error occured. "
+                + "Exception: " + e.toString()  
+                + "\nCause: " + e.getCause()
+                + "\nMessage: " + e.getMessage()
+                + "\nRestarting the program."              
+                + "\nIf this error persist, contact support."
+                + "\n\n" + e.getStackTrace()[0].toString()
+                ); 
+
+            // create StartProgramView and start the program
+            StartProgramView startProgramView = new StartProgramView();
+            startProgramView.display();
+
+        }
+
+        finally {
+            
+            try {
+                CuriousWorkmanship.inFile.close();
+                CuriousWorkmanship.outFile.close();
+                CuriousWorkmanship.logFile.close();
+                
+            } catch (IOException ex) {
+                ErrorView.display("CuriousWorkmanship", "Error closing files");
+            }
+
+        }
         
     }
 
