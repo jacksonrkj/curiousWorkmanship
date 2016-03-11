9c356757b9cd248ef825ad6194c95387bfb258a4
R. Kent Jackson <jacksonk@byui.edu>
2016-03-11 11:31:12 -0700

    3-11-2016 Fixed units of measure for each item.

--- curiousWorkmanshipChapter10/src/citbyui/cit260/curiousworkmanship/enums/Item.java
+++ curiousWorkmanshipChapter10/src/citbyui/cit260/curiousworkmanship/enums/Item.java
@@ -12,19 +12,38 @@ package citbyui.cit260.curiousworkmanship.enums;
 
 
 public enum Item {    
-    lumber,
-    ore,
-    grain,
-    legume,
-    oil,
-    water,
-    honey,
-    salt,
-    axe,
-    hammer,
-    drill,
-    shovel,
-    sickle,
-    saw,
-    nails;
+    lumber("raw", "feet"),
+    ore("raw", "tons"),
+    grain("food", "pounds"),
+    legume("food", "pounds"),
+    oil("food", "gallons"),
+    water("food", "gallons"),
+    honey("food", "gallons"),
+    salt("food", "pounds"),
+    axe("tool",""),
+    hammer("tool", ""),
+    drill("tool", ""),
+    shovel("tool", ""),
+    sickle("tool", ""),
+    saw("tool", ""),
+    nails("tool", "pounds");
+    
+    private String itemType;
+    private String unitsOfMeasure;
+
+    private Item(String itemType, String unitsOfMeasure) {
+        this.itemType = itemType;
+        this.unitsOfMeasure = unitsOfMeasure;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public String getUnitsOfMeasure() {
+        return unitsOfMeasure;
+    }
+
+  
+    
 }
